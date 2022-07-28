2d0293cd73674c589305b4fb7f064c53d544445e
Samuellsam <Samuellsam@users.noreply.github.com>
2022-07-29 01:13:33 +0700

    Filled temporary file

--- src/pages/Example1.tsx
+++ src/pages/Example1.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import { Button } from '@mui/material';
 
 const Example1: React.FC = () => {
-  return <p>Example1</p>;
+  return <Button variant="contained">adasd</Button>;
 };
 
 export default Example1;
---
+++ src/styles/useHomepageStyles.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const useHomepageStyles = () => {
+  return <div>useHomepageStyles</div>;
+};
+
+export default useHomepageStyles;
